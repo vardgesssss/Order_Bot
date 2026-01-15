163a204d150da545b1e4cf8ff4f4d0068168f64b
vardgesssss <vardgesss.grigoryan@gmail.com>
2026-01-15 19:49:28 +0300

    Update states.py

--- app/states.py
+++ app/states.py
@@ -2,12 +2,12 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class CheckoutState(StatesGroup):
-    delivery_method = State()  # 'самовывоз' или 'доставка'
-    delivery_date = State()    # выбор даты
-    delivery_time = State()    # выбор времени
+    delivery_method = State()
+    delivery_date = State()
+    delivery_time = State()
     full_name = State()
     phone = State()
-    address = State()          # используется только если delivery_method == 'доставка'
+    address = State()
     comment = State()
 
 
